647046f7744ec8a4f7d60a5ed4a3fdee32b29449
rikku-smile <nataliagg1212@gmail.com>
2025-11-02 16:33:53 -0500

    task3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+let booksobject = Object.values(books)
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -23,15 +24,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
   // Based on this: https://discussions.edx.org/course-v1:IBM+CAD220EN+3T2022/posts/674f891d7febe50470781ddc
   // The ISBN refers to the number of the book of the bookds.js file
   const isbn=req.params.isbn;
-  return res.send(books[isbn]);
+    if (books[isbn]) {
+    return res.send(books[isbn]);
+    }
+    return res.status(300).json({message: "ISBN " + isbn + "not found"});
 //   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author=req.params.author;
-    return res.send(books[author]);
-  //return res.status(300).json({message: "Yet to be implemented"});
+    let chosen_books = booksobject.filter((book) => 
+        book.author === author);
+    if (chosen_books.length != 0) {
+        return res.send(chosen_books);
+    }
+    return res.status(300).json({message: "Author " + author + "not found"});        
+//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
