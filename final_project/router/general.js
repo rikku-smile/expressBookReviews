1dce2e5e6a214498803fec7447f5949f4ee5f7f9
rikku-smile <nataliagg1212@gmail.com>
2025-11-02 14:40:11 -0500

    task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author=req.params.author;
+    return res.send(books[author]);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
