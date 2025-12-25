990df001f926d9eb33803cccb31e3efd9e9564ec
rikku-smile <nataliagg1212@gmail.com>
2025-12-25 14:09:13 -0500

    reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,7 +60,13 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+    if (books[isbn]) {
+        res.send(books[isbn].reviews);
+    }
+    return res.status(300).json({message: "ISBN " + isbn + " not found"});
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
